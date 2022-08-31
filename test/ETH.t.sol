621af1c5a6148a82cca9833f79771a3cf1e23de0
Bradley Woolf <bradleymwoolf@gmail.com>
2022-08-31 13:11:33 -0700

    foundry init

---
+++ test/ETH.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+// import {console2} from "forge-std/console2.sol";
+
+contract ContractTest is Test {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
